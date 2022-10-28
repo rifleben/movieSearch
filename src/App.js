7c0ca7117587afd20a78151a7ed77caf033bd841
Benjamin Rifleman <102486084+rifleben@users.noreply.github.com>
2022-10-28 11:19:57 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import SeachIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com?apikey=c7453c8a";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("")
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    console.log(data)
+  };
+  useEffect(() => {
+    searchMovies("");
+  }, []);
+  return (
+    <div className="app">
+      <h1>Ben's Movie Search</h1>
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img src={SeachIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
